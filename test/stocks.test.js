719018c61c388e6dca89f343843bd81f4c449fdc
makiichikawa <punkfolkfolk@gmail.com>
2021-06-02 21:27:21 +0900

    APIに対して重複したデータの要求と存在しない銘柄データの要求をしないように修正

--- test/stocks.test.js
+++ test/stocks.test.js
@@ -2,6 +2,32 @@ const Stocks = require('../lib/stocks')
 
 test('getstocks symbol 7928', async () => {
   const stocks = new Stocks(7928)
-  const data = await stocks.getRecentStocks(1)
-  await expect(data['7928.T'][0].close).toBe(1062)
+  return stocks.getRecentStocks(1).then((result) => {
+    expect(result['7928.T'][0].close).toBe(1041)
+  })
+})
+
+test('getSymbols food', async () => {
+  const stocks = new Stocks(2004)
+  const result = await stocks.getSymbols(2004)
+  expect(result).toContain('2004.T')
+})
+
+test('createNumbers (1300, 1304)', () => {
+  const stocks = new Stocks('food')
+  expect(stocks.createNumbers(1300, 1304)).toEqual([1300, 1301, 1302, 1303, 1304])
+})
+
+test.skip('autoc(num) undefind', () => {
+  const stocks = new Stocks(2005)
+  return stocks.autoc(2005).then((value) => {
+    expect(value).toBe(undefined)
+  })
+})
+
+test('autoc(num) 2004.T', () => {
+  const stocks = new Stocks(2004)
+  return stocks.autoc(2004).then((value) => {
+    expect(value).toBe('2004.T')
+  })
 })
